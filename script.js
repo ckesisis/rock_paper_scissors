6bd70c8897aaa99aa0aa863ab013888fb11fcdc3
Chris Kesisis <ckesisis@gmail.com>
2023-06-01 09:55:45 +0300

    Creating the method playRound() This method decides who player won according to player and computer choices.

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
-function getComputerChoice(){
+function getComputerChoice() {
+  let choices = ['Rock', 'Paper', 'Scissors'];
 
-    let choices = ["Rock", "Paper", "Scissors"];
-
-    console.log( choices[Math.floor(Math.random() * 3)] );
-
+  return choices[Math.floor(Math.random() * 3)];
 }
+
+function playRound( computerChoice, playerChoice ){
+
+    computerChoice = computerChoice.toLowerCase();
+    playerChoice = playerChoice.toLowerCase();
+
+    let result = '';
+
+    switch( computerChoice ){
+        case 'rock':
+            if( playerChoice === 'rock' ){
+                result = 'Tie!';
+            }else if( playerChoice === 'paper' ){
+                result = 'Player Won!';
+            }else if( playerChoice === 'scissors' ){
+                result = 'Computer Won!';
+            }
+            break;
+        case 'paper':
+            if( playerChoice === 'rock' ){
+                result = 'Computer Won!';
+            }else if( playerChoice === 'paper' ){
+                result = 'Tie!';
+            }else if( playerChoice === 'scissors' ){
+                result = 'Player Won!';
+            }
+
+            break;
+        case 'scissors':
+            if( playerChoice === 'rock' ){
+                result = 'Player Won!';
+            }else if( playerChoice === 'paper' ){
+                result = 'Computer Won!';
+            }else if( playerChoice === 'scissors' ){
+                result = 'Tie!';
+            }
+            break;
+        default :
+            result = 'No game played.';
+            break;
+    }
+    return result;
+}
+
+const playerChoice = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerChoice, computerSelection));
